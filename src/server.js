b08bf017e44388ff4a2133fea0192cb1346701d5
chitam315 <ITITIU19048@student.hcmiu.edu.vn>
2022-06-06 00:03:07 +0700

    change directory

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const router = require('./src/routes');
+const router = require('./routes');
 
 const engine = require('express-handlebars').engine
 
@@ -8,10 +8,10 @@ const app = express()
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname,'src','resources','views'));
+app.set('views', path.join(__dirname,'resources','views'));
 
-app.use(express.static(path.join(__dirname,'src','public')));
-app.use(express.static(path.join(__dirname,'src','public','css')));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public','css')));
 
 router(app)
 
