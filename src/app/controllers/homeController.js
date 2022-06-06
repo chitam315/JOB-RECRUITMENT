19e7afdbe11fc104e5770ee55fa7f161ee31a0b6
chitam315 <ITITIU19048@student.hcmiu.edu.vn>
2022-06-06 23:17:56 +0700

    login and signup page

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -1,7 +1,17 @@
 class HomeController{
-    //[GET] :/
+    //[GET] /
     index(req,res,next){
         res.render('home')
+    }
+
+    //[GET] /login
+    login(req,res,next){
+        res.render('login')
+    }
+
+    //[GET] /signup
+    signup(req,res,next){
+        res.render('signup')
     }
 }
 
--- src/routes/home.js
+++ src/routes/home.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const homeController = require('../app/controllers/homeController')
 
 router.get('/',homeController.index)
+router.get('/login',homeController.login)
+router.get('/signup',homeController.signup)
 
 module.exports = router
