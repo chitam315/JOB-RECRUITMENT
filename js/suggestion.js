f30ed67ec8e8e9668dd3c0c24f8fd540618cecbe
AnhTuan160301 <74240275+AnhTuan160301@users.noreply.github.com>
2022-06-08 01:33:49 +0700

    First

---
+++ js/suggestion.js
@@ -0,0 +1,32 @@
+let suggestion = [
+    "Financial Analyst",
+    "Freshers",
+    "Freelance",
+    "Front-end developer",
+    "Finance and Accounting",
+    "Sales",
+    "Makerting Manager",
+    "Banking",
+    "IT- Software",
+    "IT Manager",
+    "IT Helpdesk",
+    "IT Support",
+    "IT Software Developer",
+    "IT Communication",
+    "Bussiness Analyst",
+    "Mechanical Engineer",
+    "Manager",
+    "Merchandier",
+    "R&D Engineer",
+    "R&D Staff",
+    "Recruiter",
+    "Representative",
+    "Receiptionist",
+    "Web Developer",
+    "Warehouse Managerment",
+    "Warehouse Staff",
+    "Warehouse Supply"
+
+
+
+]
